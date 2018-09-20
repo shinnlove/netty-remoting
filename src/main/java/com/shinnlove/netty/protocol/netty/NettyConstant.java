673dbdb6060844de63be6e1278f849812ff082f6
chensheng.zcs <chensheng.zcs@alipay.com>
2018-09-20 18:15:15 +0800

    提交netty自定义协议代码的修改。

--- src/main/java/com/shinnlove/netty/protocol/netty/NettyConstant.java
+++ src/main/java/com/shinnlove/netty/protocol/netty/NettyConstant.java
@@ -20,6 +20,6 @@ public final class NettyConstant {
     /** 本地IP地址 */
     public static final String LOCAL_IP    = "127.0.0.1";
     /** 本地端口号 */
-    public static final int    LOCAL_PORT  = 12088;
+    public static final int    LOCAL_PORT  = 8080;
 
 }
